c712b42281e7c0cbdc1c28baca1bf60471b510aa
bamagid <bamagid60@gmail.com>
2024-01-08 15:28:26 +0000

    maj des droits d'ecritures dans les containers

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,6 @@
-sleep 10
-php bin/console doctrine:database:create --if-not-exists --no-interaction
-php bin/console doctrine:migration --no-interaction
+sleep 5
+
+php bin/console make:migration --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 
 apache2-foreground
-
